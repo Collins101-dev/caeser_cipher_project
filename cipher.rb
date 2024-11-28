a02860bd0862be7219ed3dc16830420989d67a0c
Collins Orego <collinsorego87@gmail.com>
2024-11-28 15:30:34 +0300

    added rubocop

--- cipher.rb
+++ cipher.rb
@@ -2,9 +2,9 @@ def caesar_cipher(text, shift)
   text.tr('a-z', ('a'..'z').to_a.rotate(shift).join)
 end
 
-puts "Enter text to be encrypted: "
+puts 'Enter text to be encrypted: '
 input_text = gets.chomp.downcase
-puts "Enter the number of shift: "
+puts 'Enter the number of shift: '
 input_shift = gets.chomp.to_i
 encrypted_text = caesar_cipher(input_text, input_shift)
 
